e05e7dc8e9f65c08f458c860b9fc57d6e42d5718
Yesveer <shivambhairav@gmail.com>
2024-11-10 20:53:44 +0530

    Add all the images in the services page and content

---
+++ src/app/images/index.js
@@ -0,0 +1,43 @@
+import robotics2 from './services/students.jpg';
+import robotics1 from './services/robotics1.jpg';
+import iot1 from './services/iot1.webp';
+import iot2 from './services/iot2.jpg';
+import ai1 from './services/ai1.webp';
+import ai2 from './services/ai2.jpg';
+import dataAnalysis1 from './services/dataanalysis1.jpg';
+import dataAnalysis2 from './services/dataanalysis2.jpg';
+import cyberSecurity1 from './services/cybersecurity1.jpg';
+import cyberSecurity2 from './services/cybersecurity2.jpg';
+import appdev from './services/appdev.jpg';
+import webdev from './services/webdev.webp';
+import digitalMarketing1 from './services/digitalmarketing1.webp';
+import digitalMarketing2 from './services/digitalmarketing2.jpeg';
+import designing1 from './services/desgining1.webp';
+import designing2 from './services/desgining2.avif';
+import drone1 from './services/drone1.jpg';
+import drone2 from './services/drone2.jpg';
+import printing1 from './services/printing1.jpeg';
+import printing2 from './services/printing2.png';
+
+export {
+    robotics1,
+    robotics2,
+    iot1,
+    iot2,
+    ai1,
+    ai2,
+    dataAnalysis1,
+    dataAnalysis2,
+    cyberSecurity1,
+    cyberSecurity2,
+    appdev,
+    webdev,
+    digitalMarketing1,
+    digitalMarketing2,
+    designing1,
+    designing2,
+    drone1, 
+    drone2,
+    printing1,
+    printing2,
+}
